962a4d07bca12a79ff0377d1bcef9e2008e1edbc
CGrant303 <chrisgrant303@gmail.com>
2018-09-29 12:55:14 -0500

    more initialization logic, ready to adjust per own needs

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/week18Populater", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
