2f780696fa77126250be65e67d9ef34000e76e4e
CGrant303 <chrisgrant303@gmail.com>
2018-09-30 09:59:44 -0500

    beginning styling

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get("/scrape", function (req, res) {
         });
 
         // If we were able to successfully scrape and save an Article, send a message to the client
-        res.send("Scrape Complete");
+        res.send("Scrape Complete, Please press BACK to view your articles");
     });
 });
 
